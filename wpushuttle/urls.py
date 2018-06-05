6473b85d5d4d8a32ca8ba1862cdceded51df9c22
Rodantny Reyes <rodantnyreyes@gmail.com>
2018-06-04 22:12:52 -0400

    code cleanup

--- wpushuttle/urls.py
+++ wpushuttle/urls.py
@@ -4,13 +4,13 @@ from django.urls import path
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+import template.views
 
 # Examples:
 # url(r'^$', 'gettingstarted.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^json', hello.views.jsontest, name='json'),
+    url(r'^$', template.views.index, name='index'),
+    url(r'^json', template.views.jsontest, name='json'),
 ]
